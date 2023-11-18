4857c19704cec53fcb04716a6843df34cdf07488
SourceSensei <source.sensei1205@gmail.com>
2023-11-18 23:12:17 +0000

    phase 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+	import '../components/Navbar.svelte';
+	import Navbar from '../components/Navbar.svelte';
+	import Hero from '../components/Hero.svelte';
+	import Feature from '../components/Feature.svelte';
+	import Stat from '../components/Stat.svelte';
+	import Tabs from '../components/Tabs.svelte';
+	import Caroussel from '../components/Caroussel.svelte';
+	import Testimonials from '../components/Testimonials.svelte';
+	import Price from '../components/Price.svelte';
+	import Contact from '../components/Contact.svelte';
+	import Footer from '../components/Footer.svelte';
+</script>
+
+<Navbar />
+<Hero />
+<Feature />
+<Stat />
+<Tabs />
+<Caroussel />
+<Testimonials />
+<Price />
+<Contact />
+<Footer />
